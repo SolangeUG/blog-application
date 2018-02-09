07366856e4e2197ef2765e4f2fb410d1f46c4284
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-02-09 20:34:25 +0100

    Update main application entry point

--- sug-blog/main.py
+++ sug-blog/main.py
@@ -14,12 +14,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import jinja2
 import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), "pages")
+jinja_env = jinja2.Environment(
+                loader=jinja2.FileSystemLoader(template_dir),
+                autoescape=True)
 
 
 class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    @staticmethod
+    def render_str(template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
     def get(self):
-        self.response.write('Hello Udacity!')
+        self.render("homepage.html")
 
 
 app = webapp2.WSGIApplication([
