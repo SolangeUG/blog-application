ab690069c7671eb6952b161193293360bf16a0e6
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-28 19:42:06 +0200

    Use the new utility validator when signing up for an account

--- sug-blog/unit2/signup.py
+++ sug-blog/unit2/signup.py
@@ -1,64 +1,5 @@
-import re
 import handler.handler as handler
-
-PASSWORD_RE = re.compile(r"^.{3,20}$")
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
-
-
-def valid_username(username):
-    """
-    Validate an input username against a regular expression
-    :param username: input username
-    :return: true if the username verifies, false otherwise
-    """
-    return username and USER_RE.match(username)
-
-
-def valid_password(password):
-    """
-    Validate an input password against a regular expression
-    :param password: input password
-    :return: true if the password verifies, false otherwise
-    """
-    return password and PASSWORD_RE.match(password)
-
-
-def valid_cfpassword(password, cfpassword):
-    """
-    Validate an input password and its confirmation against a regular expression
-    :param password: input password
-    :param cfpassword: confirmed password
-    :return: the result of the verification
-    """
-    result = False
-    if valid_password(password):
-        result = password == cfpassword
-    return result
-
-
-def valid_email(email):
-    """
-    Validate an input email against a regular expression
-    :param email: input email
-    :return: true if the email verifies, false otherwise
-    """
-    return not email or EMAIL_RE.match(email)
-
-
-def validate_user(username, password, cfpassword, email):
-    """
-    Validate user input information
-    :param username: input username
-    :param password: input password
-    :param cfpassword: confirmed password
-    :param email: inoput email
-    :return: true if all the information is valid, false otherwise
-    """
-    result = valid_username(username) and valid_password(password)
-    result = result and valid_cfpassword(password, cfpassword)
-    result = result and valid_email(email)
-    return result
+import util.validator as validator
 
 
 class SignupHandler(handler.TemplateHandler):
@@ -77,7 +18,7 @@ class SignupHandler(handler.TemplateHandler):
         user_email = self.request.get('email')
         comments = self.request.get('comments')
 
-        if validate_user(username, password, cfpassword, user_email):
+        if validator.validate_user(username, password, cfpassword, user_email):
             self.redirect('/welcome?username=' + username)
         else:
             username_error = ""
@@ -85,13 +26,13 @@ class SignupHandler(handler.TemplateHandler):
             cfpassword_error = ""
             email_error = ""
 
-            if not valid_username(username):
+            if not validator.is_username_valid(username):
                 username_error = "Invalid username!"
-            if not valid_password(password):
+            if not validator.is_password_valid(password):
                 password_error = "Invalid password!"
-            if not valid_cfpassword(password, cfpassword):
+            if not validator.is_cfpassword_valid(password, cfpassword):
                 cfpassword_error = "Your passwords don't match!"
-            if not valid_email(user_email):
+            if not validator.is_email_valid(user_email):
                 email_error = "Invalid email!"
             self.render("signup.html",
                         username=username,
@@ -111,7 +52,7 @@ class WelcomeHandler(handler.TemplateHandler):
     """
     def get(self):
         user = self.request.get('username')
-        if valid_username(user):
+        if validator.is_username_valid(user):
             self.render("welcome.html", username=user)
         else:
             self.redirect('/signup')
