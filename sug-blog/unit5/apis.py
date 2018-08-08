5fa605deccd4a114120d7e2578442bb94745f5f4
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-08-08 12:57:06 +0200

    Remove unnecessary handler for JSON formatted blog entries

--- sug-blog/unit5/apis.py
+++ sug-blog/unit5/apis.py
@@ -11,24 +11,10 @@ class BlogAPIHAndler(handler.TemplateHandler):
     It displays an information page about designing a pragmatic RESTful API.
     """
     def get(self):
-        self.render("apis.html")
-
-
-class BlogJSONHandler(handler.TemplateHandler):
-    """
-    BlogHandler inherits from the hander.TemplateHandler class.
-    It retrieves blog entries from the datastrore, and returns them in JSON format
-    """
-    def get(self):
         entries = db.GqlQuery('SELECT * FROM Entry ORDER BY created DESC')
         entries_list = []
         for entry in entries:
             entries_list.append(formatter.format_entry_as_json(entry))
         content = {"content": entries_list, "date": datetime.datetime.now().strftime('%b %d, %Y - %H:%M')}
-
-        # the JSON output could be displayed directly as follows
-        # self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
-        # self.write(json.dumps(content, indent=4, separators=(',', ':')))
-
-        self.render("blog_json.html", entries=json.dumps(content, indent=4, separators=(',', ':')))
+        self.render("apis.html", entries=json.dumps(content, indent=4, separators=(',', ':')))
 
